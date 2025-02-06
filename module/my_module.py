26a5ae0353898f46c4a08ca4ff53853d0a4b643f
Ivan.K <gerc-99@outlook.com>
2025-02-06 15:02:36 +0200

    Задания по Python

---
+++ module/my_module.py
@@ -0,0 +1,14 @@
+def hello(name):
+    print(f"Hello {name}")
+def add(a,b):
+    c=a+b
+    return c
+def c_a(list_num):
+    a=0
+    for i in list_num:
+        a=a+i
+        print(i)
+    t= len(list_num)
+    t=a/t
+
+    print(a,t )
